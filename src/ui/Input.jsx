c67ab5416bed00e66fc4a2c73136e40b15173ac7
Oluwa-Laughter <ismakinde@gmail.com>
2025-03-01 20:59:31 +0100

    styling the Button, Input reusable Component

---
+++ src/ui/Input.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: var(--border-radius-sm);
+  padding: 0.8rem 1.2rem;
+  color: var(--color-grey-300);
+  background-color: var(--color-grey-0);
+  box-shadow: var(--shadow-sm);
+`;
+
+export default Input;
