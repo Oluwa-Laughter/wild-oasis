a471bc13a8fcc1a0ee99270f8dc168c425feec4d
Oluwa-Laughter <ismakinde@gmail.com>
2025-03-07 23:40:53 +0100

    update supabse integration

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,11 +1,21 @@
+import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { getCabins } from "../services/apiCabins";
 
 function Cabins() {
+  useEffect(function () {
+    getCabins().then(function () {
+      console.log("Cabins fetched");
+    });
+  }, []);
+
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
       <p>TEST</p>
+
+      <img src="https://qqjlwyndzdlriiahymvi.supabase.co/storage/v1/object/public/cabins//cabin-001.jpg" />
     </Row>
   );
 }
