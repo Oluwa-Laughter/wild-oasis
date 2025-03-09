7adb309cef2469679be690eb12690e42050554d7
Oluwa-Laughter <ismakinde@gmail.com>
2025-03-09 23:35:12 +0100

    added a side changes to react query

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -33,3 +33,21 @@ export function useMoveBack() {
   const navigate = useNavigate();
   return () => navigate(-1);
 }
+
+import { useState, useEffect } from "react";
+
+export function useLocalStorageState(initialState, key) {
+  const [value, setValue] = useState(function () {
+    const storedValue = localStorage.getItem(key);
+    return storedValue ? JSON.parse(storedValue) : initialState;
+  });
+
+  useEffect(
+    function () {
+      localStorage.setItem(key, JSON.stringify(value));
+    },
+    [value, key]
+  );
+
+  return [value, setValue];
+}
