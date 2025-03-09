af4de0f5e42ce758ba55a78ae1329be15248ceb2
Oluwa-Laughter <ismakinde@gmail.com>
2025-03-09 23:32:34 +0100

    initialize react query

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -11,3 +11,25 @@ export async function getCabins() {
 
   return data;
 }
+
+export async function getBookingsAfterDate(date) {
+  const { data, error } = await supabase
+    .from("bookings")
+    .select("created_at, totalPrice, extrasPrice")
+    .gte("created_at", date)
+    .lte("created_at", getToday({ end: true }));
+
+  if (error) {
+    console.error(error);
+    throw new Error("Bookings could not get loaded");
+  }
+
+  return data;
+}
+
+import { useNavigate } from "react-router-dom";
+
+export function useMoveBack() {
+  const navigate = useNavigate();
+  return () => navigate(-1);
+}
