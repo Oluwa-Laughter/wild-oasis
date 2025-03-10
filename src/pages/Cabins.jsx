e0c66f0ed14ec6134dce5775ecfce1ae98a73c34
Oluwa-Laughter <ismakinde@gmail.com>
2025-03-10 21:56:32 +0100

    added react query for data fetching to cabins page

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import { getCabins } from "../services/apiCabins";
+import CabinTable from "../features/cabins/CabinTable";
 
 function Cabins() {
   useEffect(function () {
@@ -11,12 +12,16 @@ function Cabins() {
   }, []);
 
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter / sort</p>
+      </Row>
 
-      <img src="https://qqjlwyndzdlriiahymvi.supabase.co/storage/v1/object/public/cabins//cabin-001.jpg" />
-    </Row>
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
