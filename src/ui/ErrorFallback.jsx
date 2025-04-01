a4a3805a7a9aff8de326dbb09e22fd9da18fcc42
Oluwa-Laughter <ismakinde@gmail.com>
2025-04-01 20:23:37 +0100

    Fixing Error using react error boundary

--- src/ui/ErrorFallback.jsx
+++ src/ui/ErrorFallback.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import Heading from "./Heading";
+import GlobalStyles from "../styles/GlobalStyles";
+import Button from "./Button";
 
 const StyledErrorFallback = styled.main`
   height: 100vh;
@@ -30,8 +33,21 @@ const Box = styled.div`
   }
 `;
 
-function ErrorFallback() {
-  return <div>Error</div>;
+function ErrorFallback({ error, resetErrorBoundary }) {
+  return (
+    <>
+      <GlobalStyles />
+      <StyledErrorFallback>
+        <Box>
+          <Heading as="h1">Something went wrong</Heading>
+
+          <Button size="large" onClick={resetErrorBoundary}>
+            Try again
+          </Button>
+        </Box>
+      </StyledErrorFallback>
+    </>
+  );
 }
 
 export default ErrorFallback;
